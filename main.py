5a6f0ce4475028bc922b45c282aa588fe3ced864
henriquetomazzz <henriquetomaz021@gmail.com>
2024-03-27 11:45:19 -0300

    cronometro final

--- main.py
+++ main.py
@@ -20,24 +20,72 @@ janela.resizable(width=FALSE, height=FALSE)
 global tempo
 global rodar
 global contador
-tempo = "00:00:10"
+global limitador
+
+limitador = 59
+tempo = "00:00:00"
 rodar = False
 contador = -5
 
+#função iniciar
 def iniciar():
   global tempo
   global contador
+  global limitador
 
   if rodar:
     if contador <= -1:
-      inicio = "Começando em ", str(contador)
+      inicio = 'Começando em ', str(contador)
       label_tempo['text'] = inicio
       label_tempo['font'] = 'Arial 10'
 
+#rodando cronômetro
   else:
-    print('Satisfeita')
-    contador += 1
+     label_tempo['font'] = 'Times 50 bold'
+     temporario = str(tempo)
+     h, m, s = map(int, temporario.split(":"))
+     h = int(h)
+     m = int(m)
+     s = int(contador)
 
+     if (s>=limitador):
+       contador = 0
+       m+=1
+       
+     s = str(0) + str(s)
+     m = str(0) + str(m)
+     h = str(0) + str(h)
+     
+     #atualizando os valores
+     temporario = str(h[-2:]) + ":" + str(m[-2:]) + ":" + str(s[-2:])
+     label_tempo['text'] = temporario
+     tempo = temporario
+
+  label_tempo.after(1000, iniciar)
+  contador += 1
+
+#função para dar inicio
+def start():
+  global rodar
+  rodar = True 
+  iniciar()
+
+#função para pausar
+def pausar():
+  global rodar 
+  rodar = False
+
+#função para reiniciar
+def reiniciar():
+  global contador
+  global tempo
+
+  #reiniciando o contador
+  contador = 0
+
+  #reiniciando o tempo
+  tempo = "00:00:00"
+  label_tempo = tempo
 
 #criando labels 
 label_app = Label(janela, text="cronômetro", fon=('Arial 10'), bg=cor1, fg=cor2)
@@ -47,13 +95,13 @@ label_tempo = Label(janela, text=tempo, fon=('Times 50 bold'), bg=cor1, fg=cor6)
 label_tempo.place(x=20, y=30)
 
 #criando botões
-botao_iniciar = Button(janela, text="Iniciar", width=10, height=2, bg=cor1, fg=cor2, font=('Ivy 8 bold'), relief='raised', overrelief='ridge')
+botao_iniciar = Button(janela, command=start, text="Iniciar", width=10, height=2, bg=cor1, fg=cor2, font=('Ivy 8 bold'), relief='raised', overrelief='ridge')
 botao_iniciar.place(x=20, y=130)
 
-botao_pausar = Button(janela, text="Pausar", width=10, height=2, bg=cor1, fg=cor2, font=('Ivy 8 bold'), relief='raised', overrelief='ridge')
+botao_pausar = Button(janela, command=pausar, text="Pausar", width=10, height=2, bg=cor1, fg=cor2, font=('Ivy 8 bold'), relief='raised', overrelief='ridge')
 botao_pausar.place(x=105, y=130)
 
-botao_reiniciar = Button(janela, text="Reiniciar", width=10, height=2, bg=cor1, fg=cor2, font=('Ivy 8 bold'), relief='raised', overrelief='ridge')
+botao_reiniciar = Button(janela, command=reiniciar, text="Reiniciar", width=10, height=2, bg=cor1, fg=cor2, font=('Ivy 8 bold'), relief='raised', overrelief='ridge')
 botao_reiniciar.place(x=190, y=130)
 
 janela.mainloop()
